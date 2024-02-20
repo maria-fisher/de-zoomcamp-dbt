ab914b8e34cafb8cefbd306091f481effb588c27
M Fisher <amzriversi@gmail.com>
2024-02-20 12:38:11 +0000

    changes to staging sql files

--- models/staging/stg_yellow_taxi_data.sql
+++ models/staging/stg_yellow_taxi_data.sql
@@ -46,7 +46,7 @@ where rn = 1
 
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var('is_test_run', default=true) %}
+{% if var('is_test_run', default=false) %}
 
   limit 100
 
