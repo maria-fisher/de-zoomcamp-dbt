834943e57ff0d94a0a1631863ef8c644c834b658
M Fisher <amzriversi@gmail.com>
2024-02-20 12:03:10 +0000

    changes to schema

--- models/core/dim_zones.sql
+++ models/core/dim_zones.sql
@@ -2,5 +2,5 @@ SELECT
     location_id,
     borough,	
     zone,
-    replace(service_zone,'Boro','FHV') as service_zone 
+    replace(service_zone,'Boro','Green') as service_zone 
 FROM {{ ref ('taxi_zone_lookup')}}
